c0a2e7b31c925e987c9600a97c37889c9043befb
Jwallac3 <57156782+Jwallac3@users.noreply.github.com>
2019-10-30 19:34:32 -0500

    Add files via upload

---
+++ JasmineWallaceRHW.R
@@ -0,0 +1,11 @@
+#Jasmine Wallace Take Home Assignment October 31,2019
+# clear console, global environment 
+rm(list = ls())
+cat("\014")
+
+library(ggplot2)
+data(diamonds)
+
+head(diamonds, n=6)
+
+# This is the end of the script.
