41dc1194b67303bf4dea9ae8550449de74e392d8
Joshua-Yukich <56749858+Joshua-Yukich@users.noreply.github.com>
2019-11-02 00:17:45 -0500

    add comments and grade to TH3 script add demo script TH3

--- JasmineWallaceRHW.R
+++ JasmineWallaceRHW.R
@@ -9,3 +9,14 @@ data(diamonds)
 head(diamonds, n=6)
 
 # This is the end of the script.
+
+# all the elements are here, but strongly recommend that you actually follow the preamble exmaple given more 
+# faithfully to include a bit more info on what is happening, including the run date for example and the 
+# session info too. 
+
+# this works and is 100% credit, but I did notice that you loaded the Rscript via the file uploader 
+# rather than pushing your changes from a local copy of the repository. If you arent using this 
+# system then you are missing an opportunity for having version control and using the GitHub site 
+# more like dropbox or other file sharing. Its a wasted opportunity
+
+# I've also added my demo script to your repo so you can see an example of this. 
